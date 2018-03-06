051e3257cf2e0f791f3b25fee3662159b771b544
LisaTsai <tsaitsai225@gmail.com>
2018-03-06 11:26:26 +0800

    bg

--- bgget.py
+++ bgget.py
@@ -1,13 +1,9 @@
 from picamera import PiCamera
 from picamera.array import PiRGBArray
-import imutils
-import cv2
-import time
+from time import sleep
 
 camera=PiCamera()
 camera.resolution=(640,480)
-camera.framerate = FPS
-camera.rotation = 0
 sleep(0.5)
 
 camera.capture('/home/pi/bg.jpg')
