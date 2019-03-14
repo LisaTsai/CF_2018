51b270aefaefb178e0f83fe5534b0ec9e5f63961
LisaTsai <tsaitsai225@gmail.com>
2019-03-14 15:31:16 +0800

    Update to python3

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 sudo pip install imutils
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -y install python-opencv
-sudo apt-get -y install python-mysqldb
-python bgget.py
-python NTU_CF_ver5.py
+sudo apt-get install python3-dev libmysqlclient-dev
+sudo pip3 install mysqlclient
+sudo pip3 install sensor
+python3 bgget.py
+python3 NTU_CF_YOLOv1.py
